bcc71d9076e53e2976da6dfb08186204ea9abd94
Charlie Delgado <charliedelgado12@icloud.com>
2023-10-19 14:02:16 -0400

    feat: Introducing the initial project design

--- next-intro1/src/app/page.tsx
+++ next-intro1/src/app/page.tsx
@@ -2,8 +2,29 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Charlie Delgado 2021-1092</h1>
-    </main>
+    <div className='bg-yellow-400 min-h-screen flex flex-col'>
+      <header className='flex justify-between items-center w-full mx-auto my-4 max-w-4xl'>
+        <h1 className='text-black text-xl font-bold'>
+         🗳️Vota.dev
+        </h1>
+      </header>
+      <section className='mt-48'>
+        <h1 className='text-black text-center font-extrabold text-8xl'>
+          Vota los mejores del desarrollo web
+        </h1>
+        <p className='max-w-lg mx-auto text-center text-2xl text-black opacity-80'>
+          Decide que stack, tecnologias y frameworks han sido los mejores del año, cada voto cuenta.
+        </p>
+        <div className='bg-white h-screen max-w-3xl p-10 mx-auto mt-20 rounded-lg'>
+          <h2>Mejor framework</h2>
+          <ul>
+            <li>React</li>
+            <li>Wue</li>
+            <li>Angular</li>
+
+          </ul>
+        </div>
+      </section>
+    </div>
   )
 }
