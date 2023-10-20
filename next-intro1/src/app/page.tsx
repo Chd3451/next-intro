f143b269eb181d7f422dd3733ef714919a29bf6e
Enmanuel Sanchez Abarua <enmanuel.sanchez.a@gmail.com>
2023-10-19 20:24:14 -0400

    fix: center the page title

--- next-intro1/src/app/page.tsx
+++ next-intro1/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -8,12 +7,12 @@ export default function Home() {
          🗳️Vota.dev
         </h1>
       </header>
-      <section className='mt-48'>
-        <h1 className='text-black text-center font-extrabold text-8xl'>
+      <section className='mt-48 max-w-[1000px] mx-auto'>
+        <h1 className='text-black text-center font-extrabold text-8xl mb-10'>
           Vota los mejores del desarrollo web
         </h1>
-        <p className='max-w-lg mx-auto text-center text-2xl text-black opacity-80'>
-          Decide que stack, tecnologias y frameworks han sido los mejores del año, cada voto cuenta.
+        <p className='max-w-[600px] mx-auto text-center text-2xl text-black opacity-80'>
+          Decide qué stack, tecnologías y frameworks han sido los mejores del año. ¡Cada voto cuenta!
         </p>
         <div className='bg-white h-screen max-w-3xl p-10 mx-auto mt-20 rounded-lg'>
           <h2>Mejor framework</h2>
