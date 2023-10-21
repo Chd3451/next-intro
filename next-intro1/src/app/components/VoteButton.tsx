32c4ee6b222786116a7370cbf8e81768d10fcd76
Charlie Delgado <charliedelgado12@icloud.com>
2023-10-21 09:17:57 -0400

    fix: Vote Button  Binding element 'X' implicitly has an 'any' type [Solved]

--- next-intro1/src/app/components/VoteButton.tsx
+++ next-intro1/src/app/components/VoteButton.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-export const VoteButton = ({ children, buttonLabel }) => {
+interface VoteButtonProps {
+  children: ReactNode;
+  buttonLabel: string;
+}
+
+export const VoteButton: React.FC<VoteButtonProps> = ({ children, buttonLabel }) => {
   return (
     <button className="pointer group">
-      <div className="flex flex-row items-center gap-2 px-8 py-3 border border-gray-200 rounded-3xl ">
+      <div className="flex flex-row items-center gap-2 px-8 py-3 border border-gray-200 rounded-3xl">
         <figure className="relative flex object-contain w-10 h-10">
           {children}
         </figure>
